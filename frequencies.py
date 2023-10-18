b0cd6ac8a72f981a2b94421fd8e83cbd13a68ad5
Malik Khan <malik.khan@kcl.ac.uk>
2023-10-18 15:31:51 +0100

    finished task@

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,10 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+
+    for i in items:
+        if i in frequencies.keys():
+            frequencies[i] = frequencies.get(i) + 1
+        else:
+            frequencies[i] = 1
     return frequencies
