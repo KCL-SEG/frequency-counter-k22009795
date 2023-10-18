df65494671165a196626f0954faad109056a8e17
Malik Khan <malik.khan@kcl.ac.uk>
2023-10-18 18:18:07 +0100

    Finished

--- frequencies.py
+++ frequencies.py
@@ -6,8 +6,14 @@ def frequencies(items):
     # Your code goes here
 
     for i in items:
+        if isinstance(i, str) is False:
+            i = str(i)
+
         if i in frequencies.keys():
             frequencies[i] = frequencies.get(i) + 1
         else:
             frequencies[i] = 1
+
     return frequencies
+
+
